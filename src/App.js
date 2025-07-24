1bfd24c533fe6cf2af6e79fed7ec4bf9fa1a0a9b
meltonsilvayotpo <melton.silva@yotpo.com>
2025-07-24 22:28:32 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "";
+  let ProductId = "KRSJ003T";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
